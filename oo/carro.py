e1e63cbc22565587c16885d7ec4ec152d8f39ebd
sandrotcosta <sandrotcosta@gmail.com>
2018-04-13 11:58:18 -0300

    Composição

---
+++ oo/carro.py
@@ -0,0 +1,26 @@
+""""
+  você deve criar uma classe que vai possuir dois atributos compostos por outras duas classes:
+  1) Motor
+  2) Direção
+  
+  O motor terá a responsabilidade de controlar a velocidade 
+  Ele oferece os seguintes atributos
+  1) Atributo de dado velocidade
+  2) Método acelerar, que deverá incrementar a velocidade de uma unidade
+  3) Método frear que deverá decrementar a velocidade em duas unidades
+  
+  A direção terá a responsabilidade de controlar a direção. Ela oferece 
+  os seguintes atributos:
+  1) Valor de direção com valore possiveis: Norte,sul,leste,Oeste
+  2)Método girar_a_direita
+  3)Método girar_a_esquerda
+    
+     N
+   O   L
+     S  
+  
+  Exemplo
+    
+     >>> motor = Motor()   
+  
+""

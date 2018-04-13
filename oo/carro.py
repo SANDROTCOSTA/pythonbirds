2dd13ab3663f91f046616d8abfeea9cc3631d059
sandrotcosta <sandrotcosta@gmail.com>
2018-04-13 15:45:41 -0300

    Composição - criação

--- oo/carro.py
+++ oo/carro.py
@@ -20,7 +20,103 @@
      S  
   
   Exemplo
-    
-     >>> motor = Motor()   
-  
+    # >>> testando motor
+    >>> motor = Motor()
+    >>> motor.velocidade
+    0
+    >>> motor = Motor()
+    >>> motor.velocidade
+    1
+    >>> motor = Motor()
+    >>> motor.velocidade
+    2
+    >>> motor = Motor()
+    >>> motor.velocidade
+    3
+    >>> motor = Motor()
+    >>> motor.frear
+    1
+    >>> motor = Motor()
+    >>> motor.frear
+    0
+    # >>> testando direcao
+    >>> direcao = Direcao()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_direita()
+    >>> direcao.valor
+    'Norte'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Oeste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Sul'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Leste'
+    >>> direcao.girar_a_esquerda()
+    >>> direcao.valor
+    'Norte'
+    >>> carro = Carro(direcao, motor)
+    >>> carro.calcular_velocidade()
+    0
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    1
+    >>> carro.acelerar()
+    >>> carro.calcular_velocidade()
+    2
+    >>> carro.frear()
+    >>> carro.calcular_velocidade()
+    0
+    >>>carro.calcular_direcao()
+    'Norte'
+    >>>carro.girar_a_direita()
+    >>>carro.calcular_direcao()
+    'Leste'
+    >>>carro.girar_a_esquerda()
+    >>>carro.calcular_direcao()
+    'Norte'
+    >>>carro.girar_a_esquerda()
+    >>>carro.calcular_direcao()
+    'Oeste'
+
+
+
+
+
 ""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
